017fe15b4dd3341e5264cbc9657f73722446dbe3
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-08-17 14:44:38 +0300

    navigation

---
+++ src/components/navigation/BaseNavigation.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { INavigationRoute } from '@/interfaces/INavigationRoute.ts';
+
+defineProps<{
+  routes: INavigationRoute[];
+  vertical?: boolean;
+}>();
+</script>
+
+<template>
+  <nav class="nav">
+    <ul :class="{ nav__list_vertical: vertical }" class="nav__list">
+      <li v-for="route in routes" :key="route.id" class="nav__item">
+        <RouterLink :to="{ name: route.name }" class="nav__link">{{ route.text }}</RouterLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped lang="scss">
+.nav {
+  &__list {
+    display: flex;
+    gap: 20px;
+    &_vertical {
+      flex-direction: column;
+    }
+  }
+
+  &__link {
+    text-decoration: none;
+    padding: 3px 5px;
+    border-radius: 4px;
+    border: 2px solid $primary;
+    color: black;
+  }
+}
+</style>
