4d1e55d12742121e7a3bc19aea3269827e472bac
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-08-17 14:41:33 +0300

    axios

---
+++ src/axios/index.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { BASEURL } from '@/axios/index.const.ts';
+
+export const api = axios.create({
+  baseURL: BASEURL,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  transformRequest: [
+    function (data) {
+      return JSON.stringify(data, (key, value) => {
+        if (value === '') {
+          return undefined;
+        }
+        return value;
+      });
+    },
+  ],
+});
+api.interceptors.request.use((req) => {
+  req.headers.Authorization = 'Bearer ' + localStorage.getItem('token');
+  return req;
+});
