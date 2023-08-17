e4c1ab378e084bd15e49c5bd5384423a44320a91
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-08-17 14:41:45 +0300

    del supabase

--- src/lib/supabase.client.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-import {ISupabase} from "@/interfaces/ISupabase.ts";
-
-export const supabase = createClient<ISupabase>(
-    import.meta.env.VITE_SUPABASE_URL,
-    import.meta.env.VITE_SUPABASE_KEY
-)
