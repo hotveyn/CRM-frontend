53ab4b39b6b190e1ff75cec951ba0de999c06f3f
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-08-17 14:41:24 +0300

    eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  extends: ['eslint:recommended', '@vue/typescript/recommended', 'plugin:vue/essential', 'prettier'],
+  plugins: [],
+  rules: {
+    'vue/no-v-model-argument': 0
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
-import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
-    css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: `@import "@/assets/css/_usage.scss";`
-            }
-        }
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/assets/css/_usage.scss";`,
+      },
     },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-            'assets': path.resolve(__dirname, './src/assets'),
-            'components': path.resolve(__dirname, './src/components'),
-        },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      assets: path.resolve(__dirname, './src/assets'),
+      components: path.resolve(__dirname, './src/components'),
     },
-    plugins: [vue()],
-})
+  },
+  plugins: [vue()],
+});
