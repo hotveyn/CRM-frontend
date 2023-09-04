68d0cb0cdef107666bcec7682cb7816533ad2315
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-09-04 04:18:02 +0300

    cards

---
+++ src/components/card/order/new/CardStageNew.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { IStage } from '@/interfaces/stage/IStage.ts';
+import { NCard, NButton } from 'naive-ui';
+import { useStageNewStore } from '@/store/stages/stagesNew.ts';
+import { useDialogService } from '@/services/dialog.service.ts';
+
+defineProps<{
+  stage: IStage;
+}>();
+
+const { confirm } = useDialogService();
+const stageNewStore = useStageNewStore();
+
+async function claim(id: number) {
+  confirm(async () => {
+    await stageNewStore.claim(id);
+  });
+}
+</script>
+
+<template>
+  <NCard :title="stage.order.name">
+    <template #header-extra>{{ stage.order.code }}</template>
+    {{ stage.department.name }}
+    <template #action>
+      <NButton @click="claim(stage.id)" style="width: 100%" type="success">Принять в работу</NButton>
+    </template>
+  </NCard>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/components/card/order/work/CardStageWork.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { IStage } from '@/interfaces/stage/IStage.ts';
+import { NCard, NButton, NSpace, NModal } from 'naive-ui';
+import { useStageWorkStore } from '@/store/stages/stagesWork.ts';
+import { computed, reactive } from 'vue';
+import { useDialogService } from '@/services/dialog.service.ts';
+import FormStageBreak from 'components/forms/stage/FormStageBreak.vue';
+
+const props = defineProps<{
+  stage: IStage;
+}>();
+
+const { confirm } = useDialogService();
+const stageWorkStore = useStageWorkStore();
+
+const stageBreakModal = reactive({
+  isShow: false,
+  id: NaN,
+});
+
+const isWithBreak = computed(() => {
+  return props.stage.break_id;
+});
+
+function setBreak(id: number) {
+  stageBreakModal.isShow = true;
+  stageBreakModal.id = id;
+}
+
+function setStop(id: number) {
+  confirm(async () => {
+    await stageWorkStore.setStop(id);
+  });
+}
+
+function setBreakDone() {
+  stageBreakModal.isShow = false;
+}
+
+function ready(id: number) {
+  confirm(async () => {
+    await stageWorkStore.ready(id);
+  });
+}
+</script>
+
+<template>
+  <NCard :title="stage.order.name">
+    <template #header-extra>{{ stage.order.code }}</template>
+    <div class="card-content">
+      <p v-if="stage.department">Отдел - {{ stage.department.name }}</p>
+      <p v-if="stage.order.comment">Заметка - {{ stage.order.comment }}</p>
+      <p v-if="stage.break.name">Брак - {{ stage.break.name }}</p>
+    </div>
+    <template #action>
+      <NSpace vertical>
+        <NButton @click="ready(stage.id)" style="width: 100%" type="success">Готово</NButton>
+        <NButton v-if="stage.break" @click="setStop(stage.id)" style="width: 100%" type="error">Снять с производства</NButton>
+        <NButton v-else @click="setBreak(stage.id)" style="width: 100%" type="error" ghost>Отметить брак</NButton>
+      </NSpace>
+    </template>
+
+    <NModal v-model:show="stageBreakModal.isShow">
+      <NCard style="width: 600px">
+        <FormStageBreak :id="stageBreakModal.id" @done="setBreakDone" />
+      </NCard>
+    </NModal>
+  </NCard>
+</template>
+
+<style scoped lang="scss"></style>
