88ad2ec34eadc3a602f9959e334fffc6f54bfb52
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-09-04 04:17:32 +0300

    enums

---
+++ src/enums/order/OrderStatus.enum.ts
@@ -0,0 +1,7 @@
+export enum OrderStatusEnum {
+  NEW = 'Новый',
+  IN_WORK = 'В работе',
+  BREAK = 'Брак',
+  COMPLETED = 'Готов',
+  STOP = 'Приостановлен',
+}
---
+++ src/enums/order/OrderType.enum.ts
@@ -0,0 +1,6 @@
+export enum OrderTypeEnum {
+  NEON2 = 'НЕОН 2',
+  NEON2_STREET = 'НЕОН 2 улица',
+  NEON_SMART = 'СМАРТ неон',
+  NEON1 = 'НЕОН 1',
+}
