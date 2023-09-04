1d9132f7d8b2452977ae05ebca848c776fcec02d
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-09-04 04:18:04 +0300

    Update TheHeader.vue

--- src/components/header/TheHeader.vue
+++ src/components/header/TheHeader.vue
@@ -1,8 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useDialogService } from '@/services/dialog.service.ts';
+import { useAuthStore } from '@/store/auth.store.ts';
+import { NButton } from 'naive-ui';
+
+const dialog = useDialogService();
+const authStore = useAuthStore();
+
+function logout() {
+  dialog.confirm(async () => {
+    await authStore.logout();
+  });
+}
+</script>
 
 <template>
   <header class="header">
     <h1>NeonBro</h1>
+    <NButton v-if="authStore.isAuthenticated" type="error" ghost @click="logout" size="small">Выйти</NButton>
   </header>
 </template>
 
@@ -11,5 +25,13 @@
   padding: 30px;
   height: 100%;
   border-bottom: 2px solid $primary;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+@media (width < 700px) {
+  .header {
+    padding: 15px 10px;
+  }
 }
 </style>
