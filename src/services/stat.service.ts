16366d32475506950def0a4caa8d33f144434404
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-09-09 18:40:30 +0300

    create stat service

--- src/services/stat.service.ts
+++ src/services/stat.service.ts
@@ -1,19 +1,72 @@
 import { api } from '@/axios';
-import { IStat } from '@/interfaces/stat/IStat.ts';
+import { IStatSelf } from '@/interfaces/stat/IStatSelf.ts';
+import { IStatDepartment } from '@/interfaces/stat/IStatDepartment.ts';
+import { IStatStopped } from '@/interfaces/stat/IStatStopped.ts';
+import { IStatReclamation } from '@/interfaces/stat/IStatReclamation.ts';
+import { IStatEmployee } from '@/interfaces/stat/IStatEmployee.ts';
 
 export function useStatService() {
-  async function getSelfStat() {
-    const date = new Date();
-    const dateMonthPrev = new Date();
-    dateMonthPrev.setMonth(dateMonthPrev.getMonth() - 1);
+  return {
+    async getSelfStat() {
+      const date = new Date();
+      const dateMonthPrev = new Date();
+      dateMonthPrev.setMonth(dateMonthPrev.getMonth() - 1);
 
-    const params = {
-      start: dateMonthPrev,
-      end: date,
-    };
+      const params = {
+        start: dateMonthPrev,
+        end: date,
+      };
 
-    const res = await api.get('stat/self', { params });
-    return new Map(Object.entries(res.data)) as Map<string, IStat>;
-  }
-  return { getSelfStat };
+      const res = await api.get('stat/self', { params });
+      return new Map(Object.entries(res.data)) as Map<string, IStatSelf>;
+    },
+    async getEmployeesStat() {
+      // const date = new Date();
+      // const dateMonthPrev = new Date();
+      // dateMonthPrev.setMonth(dateMonthPrev.getMonth() - 1);
+
+      const params = {
+        start: '2004-07-13T22:02:32.395Z',
+        end: '2077-07-13T22:02:32.395Z',
+      };
+
+      const res = await api.get('stat/employees', { params });
+      return res.data as IStatEmployee[];
+    },
+    async getDepartmentsStat() {
+      const date = new Date();
+      const dateMonthPrev = new Date();
+      dateMonthPrev.setMonth(dateMonthPrev.getMonth() - 1);
+
+      const params = {
+        start: dateMonthPrev,
+        end: date,
+      };
+
+      const res = await api.get('stat/departments', { params });
+      return res.data as IStatDepartment[];
+    },
+    async getOrdersStoppedStat() {
+      // const date = new Date();
+      // const dateMonthPrev = new Date();
+      // dateMonthPrev.setMonth(dateMonthPrev.getMonth() - 1);
+      //
+      const params = {
+        start: '2004-07-13T22:02:32.395Z',
+        end: '2077-07-13T22:02:32.395Z',
+      };
+
+      const res = await api.get('stat/orders/stopped', { params });
+      return res.data as IStatStopped;
+    },
+    async getOrdersReclamationStat() {
+      const params = {
+        start: '2004-07-13T22:02:32.395Z',
+        end: '2077-07-13T22:02:32.395Z',
+      };
+
+      const res = await api.get('stat/orders/reclamation', { params });
+      return res.data as IStatReclamation;
+    },
+  };
 }
