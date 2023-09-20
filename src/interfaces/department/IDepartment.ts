a09da5ad703f13dc82eabe8352e8adf6274a4535
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-09-20 12:38:17 +0300

    add order stage entity

--- src/interfaces/department/IDepartment.ts
+++ src/interfaces/department/IDepartment.ts
@@ -1,7 +1,9 @@
 import { IUser } from '@/interfaces/user/IUser.ts';
+import {IStage} from "@/interfaces/stage/IStage.ts";
 
 export interface IDepartment {
   id: number;
   name: string;
   users?: IUser[];
+  orderStages: IStage[];
 }
