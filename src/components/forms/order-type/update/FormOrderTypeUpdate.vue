dc7cf350efef53cfa117907d15993d2b881be661
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-12-10 23:25:49 +0300

    add order type update form

---
+++ src/components/forms/order-type/update/FormOrderTypeUpdate.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { FormInst, NButton, NForm, NFormItem, NInput } from 'naive-ui';
+import { ref } from 'vue';
+import { useOrderTypesStore } from '@/store/orderTypes.store.ts';
+import { useOrderTypeUpdateFormConf } from 'components/forms/order-type/update/orderTypeUpdate.formconf.ts';
+import { IOrderType } from '@/interfaces/order-type/IOrderType.ts';
+
+const props = defineProps<{
+  orderType: IOrderType
+}>();
+
+const orderTypeFormRef = ref<FormInst | null>();
+const orderTypesStore = useOrderTypesStore();
+const { rules, formValues } = useOrderTypeUpdateFormConf(props.orderType.name);
+
+
+async function goUpdate() {
+  orderTypeFormRef.value?.validate(async (errors) => {
+    if (errors) {
+      return;
+    }
+
+    await orderTypesStore.update(props.orderType.id, formValues);
+  });
+}
+</script>
+
+<template>
+  <NForm ref="orderTypeFormRef" :rules="rules" :model="formValues" class="order-type-create-form">
+    <NFormItem label="Название типа вывески" path="name">
+      <NInput v-model:value="formValues.name" placeholder="НЕОН" />
+    </NFormItem>
+    <NButton type="primary" @click.prevent="goUpdate()"> Создать </NButton>
+  </NForm>
+</template>
+
+<style scoped lang="scss">
+</style>
---
+++ src/components/forms/order-type/update/orderTypeUpdate.formconf.ts
@@ -0,0 +1,26 @@
+import { IFormConf } from '@/interfaces/form/IFormConf.ts';
+import { FormRules } from 'naive-ui';
+import { reactive } from 'vue';
+
+
+export interface IOrderTypeUpdateValues {
+  name: string;
+}
+
+export function useOrderTypeUpdateFormConf(name: string): IFormConf<IOrderTypeUpdateValues>{
+  const rules: FormRules ={
+    name: {
+      required: true,
+      message: 'Введите название типа вывески',
+    }
+  }
+
+  const formValues = reactive<IOrderTypeUpdateValues>({
+    name
+  })
+
+  return {
+    rules,
+    formValues
+  }
+}
