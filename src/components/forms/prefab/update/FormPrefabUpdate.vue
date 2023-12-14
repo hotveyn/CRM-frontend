8a611d5050ea7f5c6a9865b51923f3afc8af5c6f
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-12-14 08:41:13 +0300

    Create FormPrefabUpdate.vue

---
+++ src/components/forms/prefab/update/FormPrefabUpdate.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { FormInst, NButton, NForm, NFormItem, NInput, NInputNumber, NSelect } from 'naive-ui';
+import { ref } from 'vue';
+import { usePrefabStore } from '@/store/prefab.store.ts';
+import { usePrefabUpdateFormConf } from 'components/forms/prefab/update/prefabUpdate.formconf.ts';
+import { IPrefab } from '@/interfaces/prefab/IPrefab.ts';
+
+const props = defineProps<{
+  prefab: IPrefab;
+}>();
+
+const emits = defineEmits<{
+  changed: [];
+}>();
+
+const prefabFormRef = ref<FormInst | null>();
+const prefabsStore = usePrefabStore();
+const { rules, formValues, options } = usePrefabUpdateFormConf(props.prefab);
+
+async function goCreate() {
+  prefabFormRef.value?.validate(async (errors) => {
+    if (errors) {
+      return;
+    }
+
+    await prefabsStore.update(props.prefab, formValues);
+    emits('changed');
+  });
+}
+</script>
+
+<template>
+  <NForm ref="prefabFormRef" :rules="rules" :model="formValues">
+    <NFormItem label="Название шаблона" path="name">
+      <NInput v-model:value="formValues.name" placeholder="" />
+    </NFormItem>
+    <NFormItem label="Стоимость шаблона" path="price">
+      <NInputNumber :min="0.1" v-model:value="formValues.price" placeholder="" :precision="2" />
+    </NFormItem>
+    <NFormItem label="Тип шаблона" path="type_id">
+      <NSelect v-model:value="formValues.type_id" remote :options="options" />
+    </NFormItem>
+    <NFormItem label="Комментарий к шаблону">
+      <NInput v-model:value="formValues.comment" type="textarea" placeholder="" />
+    </NFormItem>
+    <NButton type="primary" @click.prevent="goCreate()"> Создать </NButton>
+  </NForm>
+</template>
+
+<style scoped lang="scss"></style>
