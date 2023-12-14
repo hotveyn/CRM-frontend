59c41e375d398174a07a692666f51c06dc573014
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-12-14 08:41:26 +0300

    Create PageAdminPrefabs.vue

---
+++ src/pages/admin/prefabs/PageAdminPrefabs.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { INavigationRoute } from '@/interfaces/INavigationRoute.ts';
+import BaseNavigation from 'components/navigation/BaseNavigation.vue';
+
+const routes: INavigationRoute[] = [
+  {
+    id: 1,
+    name: 'admin-prefabs-all',
+    text: 'Все шаблоны',
+  },
+  {
+    id: 2,
+    name: 'admin-prefabs-create',
+    text: 'Создать шаблон',
+  },
+];
+</script>
+
+<template>
+  <div class="admin-prefabs">
+    <BaseNavigation :routes="routes" />
+    <div class="admin-prefabs__workspace">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.admin-prefabs {
+  &__wrokspace {
+    padding: 10px 0;
+  }
+}
+</style>
