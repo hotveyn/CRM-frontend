b0e6fcf930f3ca44323aa8b7dc2e3c3b3a64175b
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-12-14 08:41:31 +0300

    Create PageAdminPrefabsCreate.vue

---
+++ src/pages/admin/prefabs/PageAdminPrefabsCreate.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { NDivider } from 'naive-ui';
+import FormPrefabCreate from 'components/forms/prefab/create/FormPrefabCreate.vue';
+</script>
+
+<template>
+  <div class="admin-prefab-create">
+    <NDivider>Добавить новый шаблон</NDivider>
+    <FormPrefabCreate />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.admin-prefab-create {
+  display: grid;
+  width: 600px;
+  margin: 0 auto;
+}
+</style>
