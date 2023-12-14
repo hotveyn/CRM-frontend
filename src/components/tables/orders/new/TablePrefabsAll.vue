e0ec2102bfc7497d10d6962be2e11a789ac24648
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-12-14 08:41:20 +0300

    Create TablePrefabsAll.vue

---
+++ src/components/tables/orders/new/TablePrefabsAll.vue
@@ -0,0 +1,97 @@
+<script setup lang="ts">
+import { IPrefab } from '@/interfaces/prefab/IPrefab.ts';
+import { DataTableColumns, NButton, NDataTable } from 'naive-ui';
+import { h } from 'vue';
+
+defineProps<{
+  tableData: IPrefab[];
+}>();
+
+const emit = defineEmits<{
+  change: [prefab: IPrefab];
+  remove: [id: number];
+}>();
+
+const columns = createColumns({
+  change: (prefab: IPrefab) => emit('change', prefab),
+  remove: (id: number) => emit('remove', id),
+});
+
+function createColumns(actions: { change: (prefab: IPrefab) => void; remove: (id: number) => void }): DataTableColumns<IPrefab> {
+  return [
+    {
+      title: 'Название',
+      key: 'name',
+      sorter: 'default' as const,
+    },
+    {
+      title: 'Тип',
+      key: 'type.name',
+      sorter: 'default' as const,
+    },
+    {
+      title: 'Стоимость',
+      key: 'price',
+      sorter: 'default' as const,
+    },
+    {
+      title: 'Комментарий',
+      key: 'comment',
+      sorter: 'default' as const,
+    },
+    {
+      title: 'Действия',
+      key: 'actions',
+      width: 110,
+      render(row: IPrefab) {
+        return [
+          h(
+            NButton,
+            {
+              type: 'default',
+              size: 'small',
+              style: {
+                marginRight: '6px',
+              },
+              onClick: () => actions.change(row),
+            },
+            {
+              default: () => 'Изменить',
+            }
+          ),
+          h(
+            NButton,
+            {
+              type: 'error',
+              size: 'small',
+              style: {
+                marginRight: '6px',
+              },
+              onClick: () => actions.remove(row.id),
+            },
+            {
+              default: () => 'Удалить',
+            }
+          ),
+        ];
+      },
+    },
+  ];
+}
+</script>
+
+<template>
+  <div class="table-prefabs-all">
+    <NDataTable
+      :single-line="false"
+      :data="tableData"
+      :columns="columns"
+      :pagination="{
+        pageSize: 30,
+      }"
+      :bordered="true"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
