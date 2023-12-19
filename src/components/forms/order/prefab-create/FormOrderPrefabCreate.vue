cefad82e291406cd04327f4a6385c014c6cf02e4
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-12-19 15:06:56 +0300

    add new forms

---
+++ src/components/forms/order/prefab-create/FormOrderPrefabCreate.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { FormInst, NButton, NDatePicker, NFormItem, NForm, NSelect } from 'naive-ui';
+import { useOrderPrefabCreateFormConf } from 'components/forms/order/prefab-create/orderPrefabCreate.formconf.ts';
+import { ref } from 'vue';
+import { useOrdersNewStore } from '@/store/orders/orders-new.store.ts';
+
+const orderNewStore = useOrdersNewStore();
+const { rules, formValues, options } = useOrderPrefabCreateFormConf();
+
+const orderPrefabCreateFormRef = ref<FormInst | null>();
+
+function goCreate() {
+  orderPrefabCreateFormRef.value?.validate(async (errors) => {
+    if (errors) return;
+
+    await orderNewStore.createByPrefab(formValues);
+  });
+}
+
+</script>
+
+<template>
+  <NForm ref="orderPrefabCreateFormRef" :rules="rules" :model="formValues">
+    <NFormItem label="Шаблон" path="prefab_id">
+      <NSelect v-model:value="formValues.prefab_id" remote :options="options" />
+    </NFormItem>
+    <NFormItem label="Дата начала" path="date_start">
+      <NDatePicker value-format="yyyy-MM-dd" v-model:formatted-value="formValues.date_start" style="width: 100%"
+                   placeholder="" type="date" />
+    </NFormItem>
+    <NFormItem label="Дата сдачи" path="date_end">
+      <NDatePicker value-format="yyyy-MM-dd" v-model:formatted-value="formValues.date_end" style="width: 100%"
+                   placeholder="" type="date" />
+    </NFormItem>
+    <NButton type="primary" @click.prevent="goCreate()"> Создать</NButton>
+  </NForm>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/forms/order/prefab-create/orderPrefabCreate.formconf.ts
@@ -0,0 +1,58 @@
+import { IFormConf } from '@/interfaces/form/IFormConf.ts';
+import { onMounted, reactive, ref, Ref } from 'vue';
+import { FormRules, SelectOption } from 'naive-ui';
+import { usePrefabStore } from '@/store/prefab.store.ts';
+import { IPrefab } from '@/interfaces/prefab/IPrefab.ts';
+
+export interface IOrderPrefabCreateValues{
+  prefab_id?: number;
+  date_start: string;
+  date_end: string;
+}
+
+export interface IOrderPrefabCreateConf extends IFormConf<IOrderPrefabCreateValues> {
+  options: Ref<SelectOption[]>;
+}
+
+export function useOrderPrefabCreateFormConf(): IOrderPrefabCreateConf {
+  const rules: FormRules = {
+    date_start: {
+      required: true,
+      message: 'Введите дату начала',
+    },
+    prefab_id: {
+      required: true,
+      message: 'Выберите шаблон',
+    },
+  };
+
+
+  const options = ref<SelectOption[]>([]);
+
+  const formValues = reactive<IOrderPrefabCreateValues>({
+    date_start: new Date().toISOString().split('T')[0] as string,
+    date_end: new Date().toISOString().split('T')[0] as string,
+    prefab_id: undefined,
+  });
+
+  onMounted(async () => {
+    const prefabStore = usePrefabStore();
+    await prefabStore.request()
+
+    options.value = prefabStore.prefabs.map((prefab: IPrefab)=>{
+      return {
+        label: prefab.name,
+        value: prefab.id
+      }
+    });
+    if (options.value[0]) {
+      formValues.prefab_id = options.value[0].value as number;
+    }
+  });
+
+  return {
+    rules,
+    options,
+    formValues,
+  };
+}
