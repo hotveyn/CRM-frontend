1fdbe25e771a263e28281b446ca2cccdf7426605
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-12-19 15:06:18 +0300

    add new tables

---
+++ src/components/tables/matrix/TableMatrix.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { DataTableColumns, NDataTable } from 'naive-ui';
+import { ITableMatrix } from '@/interfaces/table/ITableMatrix.ts';
+
+const props = defineProps<{
+  tableData: ITableMatrix[],
+  departmentColumns: DataTableColumns<ITableMatrix>
+}>();
+
+const columns = createColumns()
+
+function createColumns(): DataTableColumns<ITableMatrix> {
+  return [
+    {
+      title: 'Тип вывески',
+      key: 'order_type.name',
+      sorter: 'default' as const
+    },
+    ...props.departmentColumns,
+    {
+      title: 'Сумма',
+      key: 'sum',
+      sorter: 'default' as const
+    },
+  ];
+}
+
+</script>
+
+<template>
+  <NDataTable
+    :single-line="false"
+    :data="tableData"
+    :columns="columns"
+    :bordered="true"
+  />
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/tables/stat/payment/TablePayment.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { DataTableColumns, NDataTable } from 'naive-ui';
+import { IDepartmentPayment, IStatPayment } from '@/interfaces/stat/IStatPayment.ts';
+
+const props = defineProps<{
+  tableData: IStatPayment[],
+  departmentColumns: DataTableColumns<IDepartmentPayment>
+}>();
+
+const columns = createColumns()
+
+function createColumns(): DataTableColumns<IDepartmentPayment> {
+  return [
+    {
+      title: '',
+      key: 'employee_name',
+    },
+    ...props.departmentColumns,
+    {
+      title: 'Сумма',
+      key: 'sum',
+      sorter: 'default' as const
+    },
+  ];
+}
+
+</script>
+
+<template>
+  <NDataTable
+    :single-line="false"
+    :data="tableData"
+    :columns="columns"
+    :bordered="true"
+  />
+</template>
+
+<style scoped lang="scss">
+
+</style>
