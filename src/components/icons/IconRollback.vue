c43448726c66618a52f439a1077b8879dc8fa83b
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2024-01-05 00:58:38 +0300

    Create IconRollback.vue

---
+++ src/components/icons/IconRollback.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <svg class="rollback" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+       viewBox="0 0 24 24">
+    <path d="M9 13L5 9l4-4M5 9h11a4 4 0 0 1 0 8h-1" fill="none" stroke="currentColor" stroke-width="2"
+          stroke-linecap="round" stroke-linejoin="round"></path>
+  </svg>
+</template>
+
+<style scoped lang="scss">
+.rollback {
+  width: 25px;
+  opacity: .5;
+}
+</style>
